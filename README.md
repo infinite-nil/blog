f5a19dc76775ac948135c367884bf263e34d287d
João Alberto <hey@joaoalberto.dev>
2023-04-06 12:31:27 -0300

    chore: creating readme and support routes by file

---
+++ README.md
@@ -0,0 +1,20 @@
+# Blog
+
+This is my attempt to create a blog system using Rust.
+
+### Goals
+
+- Have fun;
+- Build from scratch, even the HTTP server;
+- Less dependencies as possible;
+
+### Non-goals for now
+
+- Support other HTTP methods than GET;
+
+### Roadmap
+
+- [x] Create a simple single thread HTTP server;
+- [x] handle GET requests;
+- [x] handle favicon.ico;
+- [x] handle 404;
--- src/main.rs
+++ src/main.rs
@@ -40,15 +40,35 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
 
             Content::Favicon(headers, buffer)
         }
+        "/" => {
+            let content = fs::read_to_string(format!("{}index.html", file));
+
+            match content {
+                Ok(result) => {
+                    let headers = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
+                        result.len()
+                    );
+
+                    Content::Index(headers, result)
+                }
+                Err(_) => return Err(Error),
+            }
+        }
         _ => {
-            let content = fs::read_to_string(format!("{}/{}", base, "index.html")).unwrap();
+            let content = fs::read_to_string(format!("{}.{}", file, "html"));
 
-            let headers = format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
-                content.len()
-            );
+            match content {
+                Ok(result) => {
+                    let headers = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
+                        result.len()
+                    );
 
-            Content::Index(headers, content)
+                    Content::Index(headers, result)
+                }
+                Err(_) => return Err(Error),
+            }
         }
     };
 
