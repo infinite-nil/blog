10ae3e593a3b75a548cddd77f6cad6e1dfe26db6
João Alberto <hey@joaoalberto.dev>
2023-04-15 11:27:20 -0300

    feat: handle requests for css, js and images

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,26 @@ enum Content {
     Index(String, String),
 }
 
+fn get_file_extension(path: &str) -> &str {
+    // TODO: Handle possible query params
+    let ext = path.split(".").last().unwrap();
+
+    ext
+}
+
+fn get_file_content_type(extension: &str) -> &str {
+    match extension {
+        "ico" => "image/x-icon",
+        "css" => "text/css",
+        "js" => "application/javascript",
+        "svg" => "image/svg+xml",
+        "png" => "image/png",
+        "jpg" | "jpeg" => "image/jpeg",
+        // TODO: Handle more types correctly
+        _ => "text/html",
+    }
+}
+
 fn get_content(req: Vec<String>) -> Result<Content, Error> {
     let req_parts: Vec<&str> = req[0].split(" ").collect();
 
@@ -22,8 +42,12 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
     let file_path = req_parts[1];
     let full_path = format!("{}{}", base_path, file_path);
 
-    let content = match file_path {
-        "/favicon.ico" => {
+    let extension = get_file_extension(file_path);
+
+    let content = match extension {
+        // TODO: improve
+        "ico" | "css" | "js" | "svg" | "png" | "jpg" | "jpeg" => {
+            // Handle binary
             let file = File::open(full_path).unwrap();
             let mut reader = BufReader::new(file);
             let mut buffer = Vec::new();
@@ -34,28 +58,15 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
             }
 
             let headers = format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: image/x-icon\r\nContent-Length: {}\r\n\r\n",
+                "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
+                get_file_content_type(extension),
                 buffer.len()
             );
 
             Content::Favicon(headers, buffer)
-        }
-        "/" => {
-            let content = fs::read_to_string(format!("{}index.html", full_path));
 
-            match content {
-                Ok(result) => {
-                    let headers = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
-                        result.len()
-                    );
-
-                    Content::Index(headers, result)
-                }
-                Err(_) => return Err(Error),
-            }
         }
-        _ => {
+        "html" => {
             let content = fs::read_to_string(format!("{}.{}", full_path, "html"));
 
             match content {
@@ -69,9 +80,27 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
                 }
                 Err(_) => return Err(Error),
             }
+
+        }
+        _ => {
+            let content = fs::read_to_string(format!("{}index.html", full_path));
+
+            match content {
+                Ok(result) => {
+                    let headers = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
+                        result.len()
+                    );
+
+                    Content::Index(headers, result)
+                }
+                Err(_) => return Err(Error),
+            }
+
         }
     };
 
+    
     Ok(content)
 }
 
