ab2f06cfd15c419b48accf83a559916aa67971b4
João Alberto <hey@joaoalberto.dev>
2023-04-19 21:07:12 -0300

    chore: generating sitemap.txt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::{
+    borrow::BorrowMut,
+    env::var,
     fmt::Error,
     fs::{self, File},
     io::{prelude::*, BufReader},
     net::{SocketAddr, TcpListener, TcpStream},
+    path::PathBuf,
 };
 
 enum Content {
@@ -28,6 +31,25 @@ fn get_file_content_type(extension: &str) -> &str {
         // TODO: Handle more types correctly
         _ => "text/html",
     }
+}
+
+fn get_dir_content(path: String) -> Vec<String> {
+    let mut _f: Vec<String> = Vec::new();
+
+    for entry in fs::read_dir(path).unwrap() {
+        let path: PathBuf = entry.unwrap().path().clone();
+        let is_dir: bool = path.is_dir();
+
+        if is_dir {
+            let mut _c: Vec<String> = get_dir_content(path.to_str().unwrap().to_string());
+
+            _f.append(&mut _c);
+        } else {
+            _f.push(path.to_str().unwrap().to_string())
+        }
+    }
+
+    _f
 }
 
 fn get_content(req: Vec<String>) -> Result<Content, Error> {
@@ -64,7 +86,6 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
             );
 
             Content::Favicon(headers, buffer)
-
         }
         "html" => {
             let content = fs::read_to_string(format!("{}.{}", full_path, "html"));
@@ -80,7 +101,29 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
                 }
                 Err(_) => return Err(Error),
             }
+        }
+        "txt" => {
+            let sitemap = get_dir_content(base_path);
+            let sitemap = sitemap.into_iter().filter(|f| {
+                if f.contains("html") {
+                    return true;
+                }
 
+                false
+            });
+
+            let content = sitemap
+                .collect::<Vec<String>>()
+                .join("\r\n")
+                .clone()
+                .replace("content/", "");
+
+            let headers = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n",
+                content.len()
+            );
+
+            Content::Index(headers, content)
         }
         _ => {
             let content = fs::read_to_string(format!("{}index.html", full_path));
@@ -96,11 +139,9 @@ fn get_content(req: Vec<String>) -> Result<Content, Error> {
                 }
                 Err(_) => return Err(Error),
             }
-
         }
     };
 
-    
     Ok(content)
 }
 
@@ -143,6 +184,8 @@ fn handle_connection(mut stream: TcpStream) {
 }
 
 fn main() {
+    println!("ENVS {:?}", var("TEST"));
+
     let addrs = [SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 8080))];
 
     let listener = TcpListener::bind(&addrs[..]).unwrap();
